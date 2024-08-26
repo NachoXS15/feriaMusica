3cf6f45d29156ac7b5ce1d9bd122e9cdd594b1fd
Ignacio Joaquín Pantoja <pantojaignacio642@gmail.com>
2024-08-26 11:39:38 -0300

    created index page

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,28 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import {createBrowserRouter, RouterProvider} from 'react-router-dom'
+import MemoryPlay from './pages/memory/MemoryPlay.tsx'
+import QAPlay from './pages/qa/QAPlay.tsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />
+  },
+  {
+    path: '/memory-play',
+    element: <MemoryPlay />
+  },
+  {
+    path: '/qa-play',
+    element: <QAPlay />
+  }
+])
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html", 
+    "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
+  ],
+  theme: {
+    extend: {
+      colors:{
+        "lightorange": "#ffb84c",
+        "lightgreen": "#86efac",
+        "lightpurple": "#6f70c0",
+        "purple": "#5a5ac3",
+        "orange": "#f16767",
+        "cyan": "#33bbc5"
+      }
+    },
+  },
+  plugins: [],
+}
